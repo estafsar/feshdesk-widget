3a15df621faff3d46d1a69384ea06dad0d5dd6ce
Abdurrahman <abdurrahman@estafsar.com>
2021-07-12 10:53:00 +0200

    remove unused component files, and edit build command to use ng-packager

--- projects/freshdesk-widget/src/public-api.ts
+++ projects/freshdesk-widget/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of freshdesk
  */
 
-export * from './lib/freshdesk-widget.service';
-export * from './lib/freshdesk-widget.component';
 export * from './lib/freshdesk-widget.module';
